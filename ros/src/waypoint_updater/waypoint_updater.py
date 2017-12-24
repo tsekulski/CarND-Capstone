5cfdd99343c9018865ae840671b9b4b1bb5d4015
student <tomasz.sekulski@gmail.com>
2017-12-24 13:31:41 -0800

    first version of waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -21,15 +21,45 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+'''
+My potential call flow:
+0. Extract and store base_waypoints (as they are published only once)
+1. Determine the closest waypoint ahead of the car 
+- by comparing current_pose with base_waypoints
+- extract the sequence number of this closest waypoint
+- tbd: convert from vehicle to global coordinates?
+2. Generate x = LOOKAHEAD_WPS waypoint objects ahead of the vehicle:
+- in the first step w/o any target velocity (or with constant target velocity)
+- in the second step with target velocities depeding on detected traffic lights and/or obstacles
+3. Publish the waypoint objects to the final_waypoints topic
 
+Helpful resource for transforming quaternions to Euler values:
+https://answers.ros.org/question/69754/quaternion-transformations-in-python/
+
+#type(pose) = geometry_msgs.msg.Pose
+quaternion = (
+    pose.orientation.x,
+    pose.orientation.y,
+    pose.orientation.z,
+    pose.orientation.w)
+euler = tf.transformations.euler_from_quaternion(quaternion)
+roll = euler[0]
+pitch = euler[1]
+yaw = euler[2]
+
+'''
+
+LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+DEBUGGING = True
+TARGET_SPEED_MPH = 10
+TARGET_SPEED_MPS = TARGET_SPEED_MPH / 2.24 # Convert from mph to m/s
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        self.sub_current_pose = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        self.sub_base_waypoints = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
@@ -37,16 +67,80 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.curr_pose = None
+        self.all_waypoints = None
 
         rospy.spin()
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        # Grab current position from the current_pose message
+        self.curr_pose = msg.pose
+        # Find the nearest waypoint ahead
+        	# First, find the closest waypoint
+        min_dist = 100000
+        closest_index = 0
+        for i, waypoint in enumerate(self.all_waypoints):
+        	dist = sqrt((self.curr_pose.position.x - waypoint.pose.pose.position.x) ** 2 + (self.curr_pose.position.y - waypoint.pose.pose.position.y) ** 2)
+        	if (dist < min_dist):
+        		min_dist = dist
+        		closest_index = i
+
+        	# Then, check whether the waypoint is behind, if it is, increment the index to make sure it's ahead
+        heading = math.atan2(
+        	self.all_waypoints[closest_index].pose.pose.position.y - self.curr_pose.position.y, 
+        	self.all_waypoints[closest_index].pose.pose.position.x - self.curr_pose.position.x)
+        quaternion = (
+        	self.curr_pose.orientation.x, 
+        	self.curr_pose.orientation.y, 
+        	self.curr_pose.orientation.z, 
+        	self.curr_pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+		yaw = euler[2]
+        
+        angle = abs(yaw - heading)
+
+        if angle > (math.pi / 4):        
+            closest_index += 1
+
+        # If closest_index is higher than no. of items in base_waypoints --> reset closest_index to 0
+        if closest_index >= len(self.all_waypoints):
+        	closest_index = 0
+
+        # Generate the list of waypoints to be followed
+        if (closest_index + LOOKAHEAD_WPS < len(self.all_waypoints):
+        	lookahead_waypoints = self.all_waypoints[closest_index:closest_index+LOOKAHEAD_WPS]
+		# if the slice is out of bounds:
+        else:
+        	lookahead_waypoints = self.all_waypoints[closest_index:] + self.all_waypoints[:(closest_index+LOOKAHEAD_WPS - len(self.all_waypoints))] 
+        
+        # Set target speed per waypoint
+        for waypoint in lookahead_waypoints:
+        	waypoint.twist.twist.linear.x = TARGET_SPEED_MPS
+        	# This will need to be expanded once traffic lights are recognized
+
+        # Generate the message with the list of waypoints to be followed
+        lookahead_waypoints_msg = Lane()
+        lookahead_waypoints_msg.header.frame_id = '/world'
+        lookahead_waypoints_msg.header.stamp = rospy.Time(0)
+        lookahead_waypoints_msg.waypoints = lookahead_waypoints
+
+        # Publish the list of waypoints to be followed
+        self.final_waypoints_pub.publish(lookahead_waypoints_msg)
+
+        if DEBUGGING:
+        	rospy.loginfo("lookahead_waypoints_msg published"
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        # Grab the list of all waypoints from the base_waypoints message
+        self.all_waypoints = waypoints.waypoints
+
+        # Unsubscribe since this list is published only once
+        self.sub_base_waypoints.unregister()
+
+        if DEBUGGING:
+        	rospy.loginfo("base_waypoints loaded and unsubscribed")
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
